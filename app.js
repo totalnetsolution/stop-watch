2f4bf8448e017a0d246b1f2d68253c08d26b8b16
muhammad-danish <totalnetsolution2@gmail.com>
2024-04-26 00:59:02 +0500

    index file modified

--- app.js
+++ app.js
@@ -3,3 +3,11 @@ let hours = 0;
 let minutes = 0;
 let seconds = 0;
 let milliseconds = 0;
+
+const hoursDisplay = document.querySelector('hours');
+const minutesDisplay = document.querySelector('minutes');
+const secondsDisplay = document.querySelector('seconds');
+const millisecondsDisplay = document.querySelector('milliseconds');
+const startBtn = document.querySelector('startBtn');
+const stopBtn = document.querySelector('stopBtn');
+const resetBtn = document.querySelector('resetBtn');
